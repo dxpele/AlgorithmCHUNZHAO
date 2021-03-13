1acca76463d005f44b64ed99226e6f687d88ecdb
dxpele <46188699+dxpele@users.noreply.github.com>
2021-03-13 23:33:24 +0800

    Add files via upload
    第四周作业

---
+++ Week_04/countSubstrings.java
@@ -0,0 +1,40 @@
+1.动态规划 时间复杂度为 O(N^2)   空间O(N^2)
+class Solution {
+    public int countSubstrings(String s) {
+        // 动态规划法
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        int ans = 0;
+
+        for (int j = 0; j < s.length(); j++) {
+            for (int i = 0; i <= j; i++) {
+                if (s.charAt(i) == s.charAt(j) && (j - i < 2 || dp[i + 1][j - 1])) {
+                    dp[i][j] = true;
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
+2.中心扩展法 时间复杂度为 O(N^2) 空间为O(1)
+class Solution {
+    public int countSubstrings(String s) {
+        // 中心扩展法
+        int ans = 0;
+        for (int center = 0; center < 2 * s.length() - 1; center++) {
+            // left和right指针和中心点的关系是？
+            // 首先是left，有一个很明显的2倍关系的存在，其次是right，可能和left指向同一个（偶数时），也可能往后移动一个（奇数）
+            // 大致的关系出来了，可以选择带两个特殊例子进去看看是否满足。
+            int left = center / 2;
+            int right = left + center % 2;
+
+            while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+                ans++;
+                left--;
+                right++;
+            }
+        }
+        return ans;
+    }
+}
---
+++ Week_04/minPathSum.java
@@ -0,0 +1,31 @@
+//时间复杂度 ：O(M×N) ： 遍历整个grid 矩阵元素。
+//1.二维数组
+class Solution {
+    public int minPathSum(int[][] grid) {
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[0].length; j++) {
+                if(i == 0 && j == 0) continue;
+                else if(i == 0)  grid[i][j] = grid[i][j - 1] + grid[i][j];
+                else if(j == 0)  grid[i][j] = grid[i - 1][j] + grid[i][j];
+                else grid[i][j] = Math.min(grid[i - 1][j], grid[i][j - 1]) + grid[i][j];
+            }
+        }
+        return grid[grid.length - 1][grid[0].length - 1];
+    }
+}
+//2.一维数组
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int len = grid[0].length;
+        int[] dp = new int[len];
+        dp[0] = grid[0][0];
+        for (int i = 1; i < len; i++)
+            dp[i]=dp[i-1]+grid[0][i];
+        for (int i = 1; i < grid.length; i++) {
+            dp[0] = dp[0] + grid[i][0];
+            for (int j = 1; j < len; j++)
+                dp[j] = Math.min(dp[j-1]+grid[i][j], dp[j]+grid[i][j]);
+        }
+        return dp[len-1];
+    }
+}
